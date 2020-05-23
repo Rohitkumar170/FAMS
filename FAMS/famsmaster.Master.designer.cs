4e67c501b4b9ce1fe30364200f86ad1f23da41ee
Vipul Jain <vipulcrystal@gmail.com>
2020-05-23 16:52:06 +0530

    Home Button Changes

--- FAMS/famsmaster.Master.designer.cs
+++ FAMS/famsmaster.Master.designer.cs
@@ -22,6 +22,15 @@ namespace FAMS {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// BtnHome control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl BtnHome;
+        
+        /// <summary>
         /// SPUserName control.
         /// </summary>
         /// <remarks>
